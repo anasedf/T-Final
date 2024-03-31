cee97de572d8a7f994037581f39992dc494397f1
inwchamp1337 <ppasinb@gmail.com>
2024-03-31 17:01:39 +0700

    น่าจะได้ละ

--- server/index.js
+++ server/index.js
@@ -2,19 +2,19 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
 
-import dalleRoutes from './routes/dalle.routes.js';
-
+//import dalleRoutes from './routes/dalle.routes.js';
+import apiqrcode from './routes/apiqrcode.js';
 dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express.json({limit: "50mb"}));
+app.use(express.json({ limit: "50mb" }));
 
 
-app.use('/api/v1/dalle', dalleRoutes);
-
+//app.use('/api/v1/dalle', dalleRoutes);
+app.use('/api/v1/dalle', apiqrcode);
 app.get('/', (req, res) => {
-    res.status(200).json({message: "Hello from DALL.E"})
+    res.status(200).json({ message: "INAHEE" })
 });
 
 app.listen(8080, () => {
