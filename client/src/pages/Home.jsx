46b2c216ad8b8847dfe1c91b1d8ad1a585b4f5b9
inwchamp1337 <ppasinb@gmail.com>
2024-03-31 23:15:14 +0700

    Merge branch 'ของกู' of https://github.com/anasedf/T-Final into ของกู

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {motion, AnimatePresence} from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
 import { useSnapshot } from 'valtio';
 
 import state from '../store';
@@ -13,48 +13,48 @@ import {
 } from '../config/motion';
 
 const Home = () => {
-  const snap = useSnapshot(state);
+    const snap = useSnapshot(state);
 
-  return (
-    <AnimatePresence>
-        {snap.intro && (
-            <motion.section className="home" {...slideAnimation('left')}>
-                <motion.header {...slideAnimation('down')}>
-                    <img
-                        src='./threejs.png'
-                        alt="logo"
-                        className="w-8 h-8 object-contain"
-                    />
-                </motion.header>
-
-                <motion.div className="home-content" {...headContainerAnimation}>
-                    <motion.div {...headTextAnimation}>
-                        <h1 className="head-text">
-                            LET'S <br className="xl:block hidden"></br> DO IT.
-                        </h1>
-                    </motion.div>
-                    <motion.div 
-                    {...headContainerAnimation}
-                    className="flex flex-col gap-5"
-                    >
-                        <p className="max-w-md font-normal text-gray-600 text-base">
-                            Create your unique and exlusive shirt with our brand-new 3D customization tool.
-                            <strong>Unleash your imagination</strong>{" "}
-                            and define your style.
-                        </p>
-
-                        <CustomButton
-                            type="filled"
-                            title="Customize It"
-                            handleClick={() => state.intro = false}
-                            customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+    return (
+        <AnimatePresence>
+            {snap.intro && (
+                <motion.section className="home" {...slideAnimation('left')}>
+                    <motion.header {...slideAnimation('down')}>
+                        <img
+                            src='./huehiw.png'
+                            alt="logo"
+                            className="w-8 h-8 object-contain"
                         />
+                    </motion.header>
+
+                    <motion.div className="home-content" {...headContainerAnimation}>
+                        <motion.div {...headTextAnimation}>
+                            <h1 className="head-text">
+                                QR <br className="xl:block hidden"></br> SHIRT.
+                            </h1>
+                        </motion.div>
+                        <motion.div
+                            {...headContainerAnimation}
+                            className="flex flex-col gap-5"
+                        >
+                            <p className="max-w-md font-normal text-gray-600 text-base">
+                                Create your unique and exlusive shirt with our brand-new 3D customization tool.
+                                <strong>Unleash your imagination</strong>{" "}
+                                and define your style.
+                            </p>
+
+                            <CustomButton
+                                type="filled"
+                                title="Customize It"
+                                handleClick={() => state.intro = false}
+                                customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+                            />
+                        </motion.div>
                     </motion.div>
-                </motion.div>
-            </motion.section>
-        )}
-    </AnimatePresence>
-  )
+                </motion.section>
+            )}
+        </AnimatePresence>
+    )
 }
 
 export default Home
